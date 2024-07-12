f95a888cc49b5a6ce6897554724dd754297141ed
Mr.Tetteh <danielstay73@.com>
2024-07-12 05:50:03 +0000

    update 3

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,3 +1,25 @@
+import {useEffect, useState} from "react";
+
 export default function FoodDetails({foodId}) {
-    return <div>Sarah {foodId}</div>
+    const [food, setFood]=useState({})
+    const URL = `https://api.spoonacular.com/recipes/${foodId}/information`
+    const API_KEY = "62cf6e5e25874478927de1cce145a620";
+
+    useEffect(() => {
+        async function fetchFood() {
+            const res = await fetch(`${URL}?apiKey=${API_KEY}`)
+            const data = await res.json()
+            console.log(data)
+            setFood(data)
+
+        }
+        fetchFood()
+    },[foodId])
+
+    return <div>Food Details {foodId}
+        {food.title}
+        <img src={food.image} alt=""/>
+
+    </div>
 }
+
